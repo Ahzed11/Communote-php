c3534f048b425fdafb90f55868625c146e946c45
Alexandre Zenon <a.zenon@student.uclouvain.be>
2021-03-27 22:21:37 +0100

    Fixed tailwind's purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './template/**/*.html.twig',
+    './assets/**/*.js',
   ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
