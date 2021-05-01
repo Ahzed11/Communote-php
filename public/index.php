7e1bed8a5630fbfd8328d04de1794c7bd733ee71
Alexandre Zenon <a.zenon@student.uclouvain.be>
2021-05-01 23:00:01 +0200

    heroku

--- public/index.php
+++ public/index.php
@@ -15,6 +15,15 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] === 'prod') {
+    $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+}
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+}
+
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
