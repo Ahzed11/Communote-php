69e4fcbc1d0f4aea2cad04a11e5c7d9ab09dc0ce
Ahzed11 <ahzed11@protomail.com>
2021-07-25 19:39:10 +0200

    Temporary fix for prod

--- migrations/Version20210725173707.php
+++ migrations/Version20210725173707.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210725152654 extends AbstractMigration
+final class Version20210725173707 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,13 @@ final class Version20210725152654 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "user" ADD slug VARCHAR(127) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649989D9B62 ON "user" (slug)');
+        $this->addSql('ALTER TABLE "user" ALTER slug DROP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP INDEX UNIQ_8D93D649989D9B62');
-        $this->addSql('ALTER TABLE "user" DROP slug');
+        $this->addSql('ALTER TABLE "user" ALTER slug SET NOT NULL');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -62,7 +62,7 @@ class User implements UserInterface
     private string $lastName;
 
     /**
-     * @ORM\Column(type="string", length=127, unique=true)
+     * @ORM\Column(type="string", length=127, unique=true, nullable=true)
      */
     private $slug;
 
